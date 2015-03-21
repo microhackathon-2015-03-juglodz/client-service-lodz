1c6e41115b967ded598f4fdeb7656bc89899b047
pawel <pawel@Lenovo>
2015-03-21 15:09:24 +0100

    circuit breaker and retry

--- src/main/groovy/microservices/clientservice/ClientServiceController.java
+++ src/main/groovy/microservices/clientservice/ClientServiceController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.codehaus.groovy.runtime.MethodClosure;
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -13,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.HystrixCommand.Setter;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+import com.nurkiewicz.asyncretry.AsyncRetryExecutor;
 import com.ofg.infrastructure.web.resttemplate.fluent.ServiceRestClient;
 
 
@@ -26,13 +30,21 @@ public class ClientServiceController {
 	@Autowired
 	private  ServiceRestClient serviceRestClient;
 	
+	@Autowired 
+	private AsyncRetryExecutor executor;
 
 	@RequestMapping(value="/api/client",method=RequestMethod.POST)
     public void  addClient(@RequestBody Client client) throws JsonGenerationException, JsonMappingException, IOException {
 		storage.add(client);
 		serviceRestClient
 		.forService("dupaaaaa")
+		.retryUsing(executor.withMaxRetries(5))
 		.post()
+		.withCircuitBreaker(
+				Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("ClientDetails")),
+				new MethodClosure(this, "unable to contcat reporting")
+				
+		)
 		.onUrl("/api/client")
 		.body(new ObjectMapper().writeValueAsString(client))
 		.withHeaders()
